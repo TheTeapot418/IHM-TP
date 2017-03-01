98e771312062189ea08993f16194520d2ccab5a7
Guillaume Bauer <guillaume.bauer.418@gmail.com>
2017-03-01 18:06:07 +0100

    Initial commit Qt Creator default template

---
+++ IHM-TP/main.cpp
@@ -0,0 +1,14 @@
+//HANSER Florian
+//BAUER Guillaume
+
+#include "mainwindow.h"
+#include <QApplication>
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+    MainWindow w;
+    w.show();
+
+    return a.exec();
+}
