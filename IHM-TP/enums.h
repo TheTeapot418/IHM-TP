0b5bde992017ef483fb3a99ea63bbdfd055c0609
Guillaume Bauer <guillaume.bauer.418@gmail.com>
2017-03-01 23:29:00 +0100

    Added SIde and State enums

--- IHM-TP/enums.h
+++ IHM-TP/enums.h
@@ -4,4 +4,17 @@
 #ifndef ENUMS_H
 #define ENUMS_H
 
+typedef enum {
+    UPSTREAM,
+    DOWNSTREAM
+} Side;
+
+typedef enum {
+    OPEN,
+    OPENING,
+    CLOSING,
+    CLOSED,
+    ALERT
+} State;
+
 #endif // ENUMS_H
