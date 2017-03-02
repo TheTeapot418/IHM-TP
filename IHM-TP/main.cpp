e0eca4127fa0aef3da6ab7aeeaecc6da208ef4e1
ResnahF <florian.hanser@etu.unistra.fr>
2017-03-02 17:29:05 +0100

    add include into main file and create a new BlackBox object

--- IHM-TP/main.cpp
+++ IHM-TP/main.cpp
@@ -2,12 +2,15 @@
 //BAUER Guillaume
 
 #include "mainwindow.h"
+#include "blackbox.h"
+#include "simulation.h"
 #include <QApplication>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     MainWindow w;
+    BlackBox bb;
     w.show();
 
     return a.exec();
