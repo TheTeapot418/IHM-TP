91a1fe1dfe1e1654e6a29332e6d5e7cb970514ac
Guillaume Bauer <guillaume.bauer.418@gmail.com>
2017-03-03 09:03:37 +0100

    Added conects between BlackBox and Simulation

--- IHM-TP/main.cpp
+++ IHM-TP/main.cpp
@@ -18,5 +18,19 @@ int main(int argc, char *argv[])
     //Exemple de connection entre les objet (surtout pour recup le ui)
     QObject::connect(i.getUi()->enterButton,SIGNAL(clicked()),&bb,SLOT(enter()));
 
+    //Connects between BLackBox and Simulation
+    QObject::connect(&bb, SIGNAL(emergencyStop()), &s, SLOT(emergencyStop()));
+    QObject::connect(&bb, SIGNAL(endEmergencyStop()), &s, SLOT(endEmergencyStop()));
+    QObject::connect(&bb, SIGNAL(openValve(Side)), &s, SLOT(openValve(Side)));
+    QObject::connect(&bb, SIGNAL(closeValve(Side)), &s, SLOT(closeValve(Side)));
+    QObject::connect(&bb, SIGNAL(openGate(Side)), &s, SLOT(closeGate(Side)));
+    QObject::connect(&bb, SIGNAL(closeGate(Side)), &s, SLOT(closeGate(Side)));
+    QObject::connect(&bb, SIGNAL(stopGate(Side)), &s, SLOT(stopGate(Side)));
+    QObject::connect(&bb, SIGNAL(setRedLight(Side)), &s, SLOT(setRedLight(Side)));
+    QObject::connect(&bb, SIGNAL(setGreenLight(Side)), &s, SLOT(setGreenLight(Side)));
+
+    QObject::connect(&s, SIGNAL(valveState(Side,State)), &bb, SLOT(valveState(Side,State)));
+    QObject::connect(&s, SIGNAL(gateState(Side,State,int)), &bb, SLOT(gateState(Side,State,int)));
+
     return a.exec();
 }
